afc7e3554f00a84d02fdffc53b6f36721ae2edc8
Khamsavath Joe Muongmany <Khamsavath.Muongmany@outlook.com>
2022-01-18 09:28:26 -0800

    pushing password genrator assignment

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Password-Generator
+<!-- ?This simple assignment generate a password upon the users actions. The user can chose to have Alphabets, numbers, or symbols in their password. After selecting the user preference in passwords the user have the otion of having eight to one hundred and twenty eight characters in length of a random generated password. -->
+
+![](screenshotpasswordgenerator.png)
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Assignment Code
+// Setting a variable of generteBth.
 var generateBtn = document.querySelector("#generate");
+
+// Setting a variable to an array with the lower case alphabets.
 var passwordLowerLetters = [
   "a",
   "b",
@@ -28,33 +30,49 @@ var passwordLowerLetters = [
   "y",
   "z",
 ];
+//  Creating four variables to generate numbers, use symbols and  generate a password , and a open array.
 var passwordNum = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
 var passwordSymbols = ["!", "@", "#", "%", "^", "&", "*", "(", ")"];
 var password = "generatePassword";
 var resultsArr = [];
 
+// Creating a function
 function generatePassword() {
+
+  // Created three variables to confirm and one to prompt the user for action.
   var lettersConfirm = window.confirm("Do you want letters in your password?");
-  // window.prompt("Yes or No")
+  
   var numConfirm = window.confirm("Do you want numbers in your password?");
-  // window.prompt("Yes or No")
+  
   var symbolConfirm = window.confirm("Do you want symbols in your password?");
-  // window.prompt("Yes or No")
+  
   var lengthPassword = parseInt(window.prompt("Pick a number from 8 to 128"));
+  // Logging the information to the console.
   console.log(typeof lengthPassword);
   console.log(9);
+
+  // Beginning of two if statements.
   if (lengthPassword <= 8 || lengthPassword >= 128) {
+
+    // Calling the unction.
     generatePassword();
   }
   if (!lettersConfirm && !numConfirm && !symbolConfirm) {
+
+    // A windows alert to ask user for input.
     window.alert("Please choose a number, letter, or symbol.");
+
+    // Calling the function.
     generatePassword();
   }
+  // Start of a new function, two variables amd to return the element variable.
   function random(arr) {
     var index = Math.floor(Math.random() * arr.length);
     var element = arr[index];
     return element;
   }
+
+  // Beginning of a for loop to be ran with three if statements. Created a new variable and to return the result of passwordString.
   for (let i = 0; i < lengthPassword; i++) {
     if(lettersConfirm){
         resultsArr.push(random(passwordLowerLetters))
@@ -70,7 +88,7 @@ function generatePassword() {
   return passwordString;
 }
 
-//  Write password to the #password input
+//  a function WritePassword that has two variables and a method.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
